bd0d480840580988595df0c6a40e521578d154d3
Stephan <stephan.huebsch@pm.me>
2023-09-09 18:10:08 +0200

    EPC prompt leicht geändert

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function toggleVisibility(IPType) {
 }
 
 function jumpToEPC() {
-    x = prompt("Artikel EPÜ: a87\nAusfO: r12a\nGebO: f2");
+    x = prompt("Artikel: a87\nAusfO: r12a\nGebO: f2");
     if(/[aA][1-9][0-9]{0,2}[a-z]{0,1}/.test(x)) {
         window.location = "https://www.epo.org/de/legal/epc/2020/a" + x.substring(1) + ".html"  
     }  
@@ -102,7 +102,7 @@ function jumpToEPC() {
 }
 
 function jumpToPCT() {
-    x = prompt("Artikel PCT: a19\nAusfO: r13bis\nVerwVor: s104");  
+    x = prompt("Artikel: a19\nAusfO: r13bis\nVerwV: s104");  
     if(/[aA][1-9][0-9]{0,2}[a-z]{0,9}/.test(x)){   
         window.location = "https://www.wipo.int/pct/de/texts/articles/" + x + ".html"  
     }  
