5f87101b4d9e1e7517485576ce51fc9aded0f42e
Stephan Hübsch <stephan.huebsch@pm.me>
2025-03-24 23:01:03 +0100

    ValV hinzugefügt

--- test/script.js
+++ test/script.js
@@ -100,6 +100,7 @@ document.addEventListener("DOMContentLoaded", function() {
     '<a href="pvue.html">PVÜ</a>' +
     '<a href="standesrl.html">StandesRL</a>' + 
     '<a href="ausbv.html">AusbV</a>' + 
+    '<a href="pag-valv.html">PAG-ValV</a>' + 
     '<a href="eingabenk.html">EingabenK</a>';
 
   // Get the current page's file name, e.g., "a.html"
